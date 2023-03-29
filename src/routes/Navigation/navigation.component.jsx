4f8027809b8ec9394f1a833494bbbfe6da206184
Tengevic <tengevictor7@gmail.com>
2023-03-29 16:41:51 +0300

    added navigation bar and routes

---
+++ src/routes/Navigation/navigation.component.jsx
@@ -0,0 +1,21 @@
+import {Outlet, Link} from "react-router-dom";
+import { Fragment } from "react";
+import "./navigation.styles.scss"
+
+import { ReactComponent as Crwnlog } from "../../assets/crown.svg";
+
+const Navigation = () =>{
+    return (<Fragment>
+                <div className="navigation">
+                    <Link className="logo-conatiner" to='/'>
+                     <Crwnlog/>
+                    </Link>
+                    
+                    <div className="nav-links-container">
+                        <Link className="nav-link" to="/Shop">Shop</Link>
+                    </div>
+                </div>
+                <Outlet/>
+            </Fragment>)
+  }
+  export default Navigation;
